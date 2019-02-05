0e53ade890dd42a4188759b64e3d91032df21c45
daiyeyue <150064328@qq.com>
2019-02-05 13:55:13 +0800

    End LY-04-For

--- For/LY-04-For.py
+++ For/LY-04-For.py
@@ -2,3 +2,15 @@ for i in range(0,4):
     for j in range(0,5):
         print("*" , end=" ")
     print("\t")
+
+
+
+#简单图形打印
+
+for i in range(0,4):
+    if i == 0 or i ==3 :
+        print("* "  * 5);
+
+    else:
+        print("*       *")
+
