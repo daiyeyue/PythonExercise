7d66de99cbd4de97cf3bdf111da2ce81cf306d32
daiyeyue <150064328@qq.com>
2019-02-05 16:58:11 +0800

    New LY-05-For

--- For/LY-05-For.py
+++ For/LY-05-For.py
@@ -32,3 +32,60 @@ for i in range(0,5):
         else:
             print("  ", end="")
     print()
+
+'''
+打印倒三角
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+'''
+for i in range(0,5):
+    for j in range(0,5-i):
+        print("* ", end="")
+    print()
+
+#这样也可以
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print("* ", end="")
+    print()
+
+'''
+打印倒空三角
+* * * * * 
+*     * 
+*   * 
+* * 
+* 
+'''
+for i in range(0,5):
+    for j in range(0,5-i):
+        if i == 0:
+            print("* ", end="")
+            continue
+        if j == 0 or j == 5-i-1:
+            print("* ", end="")
+        else:
+            print("  ", end="")
+    print()
+
+def triangle(n):
+    '''
+    *           
+   * *
+  * * *
+ * * * * 
+* * * * *
+    :param n: 等边三角形的行数
+    :return: 
+    '''
+    if n <= 1:
+        print("一行*无法成为等边三角形")
+    else:
+        for i in range(n,0,-1):
+            print(" " * (i - 1) + "*" + " *" * (n-i))
+
+
+triangle(5)
